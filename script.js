1edc0377ec67cf22ae070a4108f336612207b07d
GEndr1 <goxhajendri5@gmail.com>
2024-06-08 13:21:33 +0200

    Minor aesthetic change

--- script.js
+++ script.js
@@ -4,15 +4,7 @@ let gameBoard = ["", "", "", "", "", "", "", "", ""]
 function createUser(name){
     const getScore = ()=> score;
     const giveScore = ()=> score++;
-    const marker = "x"/*prompt("choose marker")*/;
-    const play = () =>{
-        let i = "";
-        i = prompt("choose position");
-        gameBoard[i] = marker;
-        checkwinner();
-        round++
-        checktie();
-    }
+    let marker;
     let score = 0;
     return{name, marker, getScore, giveScore, play}
 };
@@ -20,6 +12,34 @@ player1 = "Devi"
 player2 = "Endri"
 player1 = createUser(player1);
 player2 = createUser(player2);
+player1.marker = 'x';
+player2.marker = 'o';
+
+let turn = true;
+function play(){
+    for(i = 0;i <= 8;i++){
+        if(turn === true){
+            let j = "";
+            j =  prompt("choose position for player 1");
+            gameBoard[j] = player1.marker;
+            checkwinner();
+            round++
+            checktie();
+            console.log(gameBoard);
+            turn = !turn;
+        }
+        else if(turn === false){
+            let j = "";
+            j =  prompt("choose position for player 2");
+            gameBoard[j] = player2.marker;
+            checkwinner();
+            round++
+            checktie();
+            console.log(gameBoard);
+            turn = !turn;
+        }
+    }
+}
 
 function checktie(){
     if(round === 9 && winner === 0){
@@ -44,87 +64,105 @@ function checkwinner(){
         console.log(`${player1.name} wins`);
         player1.giveScore();
         winner++;
+        winnerd = true;
+        i = 10;
+
     }
     else if(combination1 === player2combination){
         console.log(`${player2.name} wins`);
         player2.giveScore();
         winner++;
+        i = 10;
     }
     else if (combination2 === player1combination )
     {
         console.log(`${player1.name} wins`);
         player1.giveScore();
         winner++;
+        i = 10;
     }
     else if(combination2 === player2combination){
         console.log(`${player2.name} wins`);
         player2.giveScore();
         winner++;
+        i = 10;
     }
     else if (combination3 === player1combination )
     {
         console.log(`${player1.name} wins`);
         player1.giveScore();
         winner++;
+        i = 10;
     }
     else if(combination3 === player2combination){
         console.log(`${player2.name} wins`);
         player2.giveScore();
         winner++;
+        i = 10;
     }
     else if (combination4 === player1combination )
     {
         console.log(`${player1.name} wins`);
         player1.giveScore();
         winner++;
+        i = 10;
     }
     else if(combination4 === player2combination){
         console.log(`${player2.name} wins`);
         player2.giveScore();
         winner++;
+        i = 10;
     }
     else if (combination5 === player1combination )
     {
         console.log(`${player1.name} wins`);
         player1.giveScore();
         winner++;
+        i = 10;
     }
     else if(combination5 === player2combination){
         console.log(`${player2.name} wins`);
         player2.giveScore();
         winner++;
+        i = 10;
     }
     else if (combination6 === player1combination )
     {
         console.log(`${player1.name} wins`);
         player1.giveScore();
         winner++;
+        i = 10;
     }
     else if(combination6 === player2combination){
         console.log(`${player2.name} wins`);
         player2.giveScore();
         winner++;
+        i = 10;
     }
     else if (combination7 === player1combination )
     {
         console.log(`${player1.name} wins`);
         player1.giveScore();
         winner++;
+        i = 10;
     }
     else if(combination7 === player2combination){
         console.log(`${player2.name} wins`);
         player2.giveScore();
         winner++;
+        i = 10;
     }
     else if (combination8 === player1combination )
     {
         console.log(`${player1.name} wins`);
         player1.giveScore();
         winner++;
+        i = 10;
     }
     else if(combination8 === player2combination){
         console.log(`${player2.name} wins`);
         player2.giveScore();
         winner++;
+        i = 10;
     }
 }
