d796393c43920e757a95c7ed394b3e16ffad5f8f
GEndr1 <goxhajendri5@gmail.com>
2024-06-10 12:22:29 +0200

    Added function to check for winner

--- script.js
+++ script.js
@@ -1,9 +1,90 @@
-const gridCell = document.querySelectorAll('.cell');
-let array = ["", "", "", "", "", "", "", "", ""]
-gridCell.forEach((cell) => {
-    cell.addEventListener('click', ()=>{
-        cell.textContent = "X";
-        array[cell.id] = "x"
-        console.log(array);
+const gameBoard = (function (){
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    let turn = 1;
+
+    const player1 = {
+        name: 'Player 1',
+        marker: 'X'
+    }
+    const player2 = {
+        name: 'Player 2',
+        marker: 'O'
+    }
+
+    return {
+        board,
+        turn,
+        player1,
+        player2
+    }
+})();
+const playGame = (function (){
+    const gridCell = document.querySelectorAll('.cell');
+    let isWinner = false;
+
+    gridCell.forEach((cell) => {
+        cell.addEventListener('click', ()=>{
+            if(cell.textContent == "" && isWinner === false){ // prevents user from selecting an alredy selected cell
+                if(gameBoard.turn == 1){
+                    cell.textContent = gameBoard.player1.marker;
+                    gameBoard.board[cell.id] = cell.textContent;
+                    gameBoard.turn = 2;
+                    console.log(gameBoard.board);
+                    for(i = 0; i < 8; i++){
+                        for(j = 0; j < 3; j++){
+                            let [a,b,c] = winCombinations[i];
+                            if(gameBoard.board[a] != "" && gameBoard.board[a] === gameBoard.board[b] && gameBoard.board[b] === gameBoard.board[c]){
+                                isWinner= true;
+                            }
+                        }
+                    }
+                }
+                else if(gameBoard.turn == 2){
+                    cell.textContent = gameBoard.player2.marker;
+                    gameBoard.board[cell.id] = cell.textContent;
+                    gameBoard.turn = 1;
+                    console.log(gameBoard.board);
+                    for(i = 0; i < 8; i++){
+                        for(j = 0; j < 3; j++){
+                            let [a,b,c] = winCombinations[i];
+                            if(gameBoard.board[a] != "" && gameBoard.board[a] === gameBoard.board[b] && gameBoard.board[b] === gameBoard.board[c]){
+                                isWinner = true;
+                            }
+                        }
+                    }
+                }
+            }
+            else{
+                //DO NOTHING
+            }
+        })
     })
-})
+
+    const winCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+   
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', ()=>{
+        for(i = 0; i < 8; i++){
+            for(j = 0; j < 3; j++){
+                let [a,b,c] = winCombinations[i];
+                if(gameBoard.board[a] != "" && gameBoard.board[a] === gameBoard.board[b] && gameBoard.board[b] === gameBoard.board[c]){
+                    console.log("winner");
+                }
+            }
+        }
+    })
+    return{
+        winCombinations,
+        winner
+    }
+})();
